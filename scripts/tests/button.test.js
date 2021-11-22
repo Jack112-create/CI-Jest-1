21845909c399549372286a761bb240256ffac358
Jack112-create <jackconroy777@gmail.com>
2021-11-22 12:48:42 +0000

    Add DOM mock test

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -6,12 +6,20 @@ const { beforeEach, describe, expect } = require("@jest/globals");
 const buttonClick = require("../button");
 
 beforeEach(() => {
-    document.body.innerHTML = "<p id='par'></p>"
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe("DOM tests", () => {
     test('Expects p content to change', () => {
         buttonClick();
-        expect(document.getElementById("par").innerHTML == "You Clicked");
+        expect((document.getElementById("par").innerHTML)).toEqual("You Clicked");
     });
+    test('H1 should exist', () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1)
+    });
+    
 });
